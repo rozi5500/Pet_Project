77b553b6fb9e807500b2961b640c7e551850379a
Nick <kolyabogach12@gmail.com>
2022-06-12 21:05:53 +0300

    Revert "some changes in func sumator"
    This reverts commit a2e8f74af0734a620b9e63b395b6601ed40f049d.

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const { NODE_ENV, PORT } = require('./config/config')
 console.log(process.argv);
 
 function sumator(a, b) {
-  return b + a;
+  return a + b;
 }
 
 const sumator1 = sumator(14, 10);
