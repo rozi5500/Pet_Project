9f07f4461bbdd0bfb877df65457d0a25f8fd5f9b
Nick <kolyabogach12@gmail.com>
2022-06-18 20:09:57 +0300

    study commit

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
+const func = require('./sumator.js')
+
 const dotenv = require('dotenv');
 dotenv.config();
 
 const { NODE_ENV, PORT } = require('./config/config')
 
 
-console.log(process.argv);
+console.log(NODE_ENV);
+console.log(PORT);
 
-function sumator(a, b) {
-  return a + b;
-}
 
-const sumator1 = sumator(14, 10);
-console.log(sumator1);
+const sumator1 = func.sumator(5, 20);
---
+++ sumator.js
@@ -0,0 +1,7 @@
+function sumator(a, b) {
+  return a + b;
+}
+
+module.exports = {
+  sumator
+}
